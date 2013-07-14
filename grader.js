a0f7d33f5bc20c1516f6c8d61ec1e1a34fd439e7
Kalin Videv <KalinVidev@hotmail.com>
2013-07-14 02:09:32 +0000

    Finally got grader.js to grade urls

--- grader.js
+++ grader.js
@@ -21,18 +21,20 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
-
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URLFILE_DEFAULT = "http://polar-cliffs-4056.herokuapp.com";
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1);
+	console.log("%s does not exist. Exiting.", instr);
+	process.exit(1);
      // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
@@ -51,8 +53,8 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
     }
     return out;
 };
@@ -63,15 +65,34 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+
+
+
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_file>', 'URL to index.html')
+	.parse(process.argv);
+    
+    if(typeof(program.url) !== 'undefined') {
+       console.log("using URL");
+       rest.get(program.url).on('complete', function (data) {
+           $ = cheerio.load(data);
+           var checks = loadChecks(program.checks).sort();
+           var checkJson = {};
+           for(var ii in checks) {
+               var present = $(checks[ii]).length > 0;
+               checkJson[checks[ii]] = present;
+           };
+           var outJson = JSON.stringify(checkJson, null, 4);
+           console.log(outJson);
+       });
+    } else {
+       var checkJson = checkHtmlFile(program.file, program.checks);
+       var outJson = JSON.stringify(checkJson, null, 4);
+       console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
-
